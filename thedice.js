fe3e9849430823179a5d8a47f4c1661c84d24ed9
MixedPepperoni <Mixyboo@gmail.com>
2025-01-11 17:54:16 -0500

    commit lab 8

---
+++ thedice.js
@@ -0,0 +1,36 @@
+function rollDice4() {
+  alert(1 + Math.floor(Math.random() * 4));
+  return 1 + Math.floor(Math.random() * 4);
+}
+console.log(rollDice4());
+//Worked great. THis code dsiplay the random nums//
+function rollDice6() {
+  alert(1 + Math.floor(Math.random() * 6));
+  return 1 + Math.floor(Math.random() * 6);
+}
+console.log(rollDice6());
+//Worked great. THis code dsiplay the random nums//
+function rollDice8() {
+  alert(1 + Math.floor(Math.random() * 8));
+  return 1 + Math.floor(Math.random() * 8);
+}
+console.log(rollDice8());
+//Worked great. THis code dsiplay the random nums//
+function rollDice10() {
+  alert(1 + Math.floor(Math.random() * 10));
+  return 1 + Math.floor(Math.random() * 10);
+}
+console.log(rollDice10());
+//Worked great. THis code dsiplay the random nums//
+function rollDice12() {
+  alert(1 + Math.floor(Math.random() * 12));
+  return 1 + Math.floor(Math.random() * 12);
+}
+console.log(rollDice12());
+//Worked great. THis code dsiplay the random nums//
+function rollDice20() {
+  alert(1 + Math.floor(Math.random() * 20));
+  return 1 + Math.floor(Math.random() * 20);
+}
+console.log(rollDice20());
+//Worked great. THis code dsiplay the random nums//
